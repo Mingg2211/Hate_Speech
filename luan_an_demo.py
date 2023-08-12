1fb209da13f9bdf83ceb8d26c0c2bfc644803f3f
Nguyễn Phúc Toàn <toan.it.mta@gmail.com>
2023-08-13 03:04:34 +0700

    Toan update code application

--- luan_an_demo.py
+++ luan_an_demo.py
@@ -1,69 +1,36 @@
-import joblib
-import re
 import numpy as np
 import gradio as gr
-
-def lower_text(text:str):
-    return text.lower()
-
-def remove_emoji(text):
-    emoji_pattern = re.compile("["
-                               u"\U0001F600-\U0001F64F"  # emoticons
-                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                               u"\U0001F700-\U0001F77F"  # alchemical symbols
-                               u"\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
-                               u"\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
-                               u"\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
-                               u"\U0001FA00-\U0001FA6F"  # Chess Symbols
-                               u"\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
-                               u"\U0001F680-\U0001F6FF"  # Transport and Map Symbols
-                               u"\U00002702-\U000027B0"  # Dingbats
-                               u"\U000024C2-\U0001F251"
-                               "]+", flags=re.UNICODE)
-    return emoji_pattern.sub(r'', text)
-
-def preprocess(text:str):
-    text = lower_text(text)
-    text = remove_emoji(text)
-    return text.strip()
-
-def load_model():
-    vectorizer_filename = 'model/count_vectorizer.joblib'
-    model_filename = 'model/bayes.model'
-    vectorizer = joblib.load(vectorizer_filename)
-    model = joblib.load(model_filename)
-    return vectorizer, model
-
-new_text = 'Mong sao thằng pháp không sang xâm lược Việt Nam là may lắm rồi 😂 thắng hay thua kệ bà pháp đi'
-
-vectorizer , model = load_model()
-
-id2label = {
-    0: "CLEAN",
-    1: "OFFENSIVE",
-    2: "HATE"
-}
+from sentiment_bert import bert_sentiment_classification
+from sentiment_bayes import bayes_sentiment_classification
+from sentiment_gru import gru_sentiment_classification
 
 
-def sentiment_analysis(text :str):
-    text_preprocess = preprocess(text)
+css_string = "#title {text-align: center;}"
+element_id = "title"
+with gr.Blocks(css=css_string) as demo:
+    gr.Markdown(elem_id = element_id, value="# Chương trình phân tích cảm xúc")
+    with gr.Tab("Mô hình Bayes"):
+        text_input_bayes = gr.Textbox()
+        text_output_bayes = gr.Label()
+        text_button_bayes = gr.Button("Submit")
 
-    text_vectorizer = vectorizer.transform([text_preprocess]) 
-    label_pred = model.predict_proba(text_vectorizer)[0]
-    list_label = ["CLEAN", 'HATE']
-    result_dict = dict(zip(list_label, label_pred))
-    return result_dict
+    with gr.Tab("Mô hình GRU_MLP"):
+        text_input_GRU = gr.Textbox()
+        text_output_GRU = gr.Label()
+        text_button_GRU = gr.Button("Submit")
     
-# print(sentiment_analysis('dmm'))
-demo = gr.Interface(
-    fn=sentiment_analysis, 
-    inputs=gr.Textbox(placeholder="Enter a positive or negative sentence here..."), 
-    outputs="label", 
-    interpretation="default",
-    examples=[["Tuyệt vời"]])
+    with gr.Tab("Mô hình Bert_MLP"):
+        text_input_Bert = gr.Textbox()
+        text_output_Bert = gr.Label()
+        text_button_Bert = gr.Button("Submit")
 
-demo.launch()
+    text_button_bayes.click(bayes_sentiment_classification, inputs=text_input_bayes, outputs=text_output_bayes)
+    text_button_Bert.click(bert_sentiment_classification, inputs=text_input_Bert, outputs=text_output_Bert)
+    text_button_GRU.click(gru_sentiment_classification, inputs=text_input_GRU, outputs=text_output_GRU)
+
+if __name__ == "__main__":
+    demo.launch()
+
 
 
 
